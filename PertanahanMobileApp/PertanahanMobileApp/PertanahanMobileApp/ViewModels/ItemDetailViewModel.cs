1bc881a43c74855b79d8c9a1d7ddb6700bbe9fec
ocph23 <ocph23@hotmail.com>
2018-07-25 14:39:33 +0900

    First

---
+++ PertanahanMobileApp/PertanahanMobileApp/PertanahanMobileApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using PertanahanMobileApp.Models;
+
+namespace PertanahanMobileApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
